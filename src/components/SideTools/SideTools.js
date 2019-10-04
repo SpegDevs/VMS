d240a7a9c21f5126e5a4a981af003ce01b9d2cf7
petrlr14 <pedrodeveloper14@gmail.com>
2019-10-04 08:38:48 -0600

    structura basica del proyecto

---
+++ src/components/SideTools/SideTools.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SideTools.module.css";
+
+export const SideTools = props => {
+  return <div className="main">
+    
+  </div>;
+};
---
+++ src/pages/Ide.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Ide.module.css";
+
+export const Ide = () => {
+  return (
+    <>
+      <main>
+        <div class="">1</div>
+        <div>2</div>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
