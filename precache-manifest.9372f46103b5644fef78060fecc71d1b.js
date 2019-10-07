d1129d737483718a159627f83a5e555a02dbe6b2
Moemeister <00062716@uca.edu.sv>
2019-10-07 12:57:00 -0600

    Updates

---
+++ precache-manifest.9372f46103b5644fef78060fecc71d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb24294fb53b37197f55a3d0fc00e270",
+    "url": "/VMS/index.html"
+  },
+  {
+    "revision": "8cb89e2b8775d6b43d9b",
+    "url": "/VMS/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d7c4cc8b37d211884139",
+    "url": "/VMS/static/css/main.09a5092c.chunk.css"
+  },
+  {
+    "revision": "8cb89e2b8775d6b43d9b",
+    "url": "/VMS/static/js/2.13e918b7.chunk.js"
+  },
+  {
+    "revision": "d7c4cc8b37d211884139",
+    "url": "/VMS/static/js/main.e70d60d4.chunk.js"
+  },
+  {
+    "revision": "313c593d800240c0af79",
+    "url": "/VMS/static/js/runtime-main.bbdfb133.js"
+  }
+]);
